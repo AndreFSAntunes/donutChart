e79f8f5d4cccb654b669409a78a642bf71f55427
Andre Antunes <andre.antunes@live.com>
2023-06-18 20:56:09 -0300

    adicionado porcentagem no centro

--- donut.js
+++ donut.js
@@ -1,18 +1,53 @@
 window.onload = function () {
-    carregarGrafico(58, 115);
+    carregarGrafico(70, 100);
 };
 
 function carregarGrafico(atendidas, total) {
-    let ctx = document.getElementById('graficoDonut').getContext('2d');
-
-    let progresso = 0;
-    const progressoFinal = (100 * atendidas) / total;
-    const duracao = 1000;
-    const increment = progressoFinal / (duracao / 10);
+    const ctx = document.getElementById('graficoDonut').getContext('2d');
     const corAtendidos = '#00a1ff';
     const corNaoAtendidos = '#ccc';
+    let progresso = 0;
+    const progressoFinal = (100 * atendidas) / total;
+    const increment = progressoFinal / (1000 / 10); // duration: 1000
 
-    let dados = {
+    const options = {
+        cutoutPercentage: 80,
+        animation: { duration: 0 },
+        legend: { display: false },
+        responsive: false,
+        maintainAspectRatio: false,
+        tooltips: {
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    const dataset = data.datasets[tooltipItem.datasetIndex];
+                    const currentValue = dataset.data[tooltipItem.index];
+                    const label = dataset.label ? `${dataset.label}: ` : '';
+                    return label + currentValue;
+                }
+            }
+        },
+        plugins: {
+            beforeDraw: function(chart) {
+                const width = chart.chart.width;
+                const height = chart.chart.height;
+                const ctx = chart.chart.ctx;
+
+                ctx.restore();
+                ctx.font = `${(height / 5).toFixed(2)}px`;
+                ctx.fillStyle = '#000';
+                ctx.textBaseline = 'middle';
+
+                const text = `${Math.round(progresso)}%`;
+                const textX = Math.round((width - ctx.measureText(text).width) / 2);
+                const textY = height / 2;
+
+                ctx.fillText(text, textX, textY);
+                ctx.save();
+            }
+        }
+    };
+
+    const data = {
         datasets: [{
             data: [progresso, 100 - progresso],
             backgroundColor: [corAtendidos, corNaoAtendidos],
@@ -20,38 +55,32 @@ function carregarGrafico(atendidas, total) {
         }]
     };
 
-    const options = {
-        cutoutPercentage: 80,
-        animation: {
-            duration: 0
-        },
-        legend: {
-            display: false
-        },
-        responsive: false,
-        maintainAspectRatio: false
+    let graficoDonut = new Chart(ctx, { type: 'doughnut', data, options });
+
+    const updateTextoPorcentagem = () => {
+        const textElement = document.getElementById('porcentagemTexto');
+        if (!textElement) return;
+
+        const porcentagem = Math.round(progresso);
+        textElement.innerText = `${porcentagem}%`;
+        if (porcentagem >= 9.9) {
+            textElement.style.transform = 'translate(-200%, -40%) scale(1.3)';
+        }
     };
 
-    let graficoDonut = new Chart(ctx, {
-        type: 'doughnut',
-        data: dados,
-        options: options
-    });
-
-    const animacaoProgresso = function () {
-        if (progresso < progressoFinal) {
-            progresso += increment;
-            graficoDonut.data.datasets[0].data[0] = progresso;
-            graficoDonut.data.datasets[0].data[1] = 100 - progresso;
+    const animacaoProgresso = () => {
+        if (progresso >= progressoFinal) {
+            graficoDonut.data.datasets[0].data = [atendidas, total - atendidas];
             graficoDonut.update();
-            setTimeout(animacaoProgresso, 10);
-        } else {
-            graficoDonut.data.datasets[0].data[0] = atendidas;
-            graficoDonut.data.datasets[0].data[1] = total - atendidas;
-            graficoDonut.update();
+            return;
         }
 
-    }
+        progresso += increment;
+        graficoDonut.data.datasets[0].data = [progresso, 100 - progresso];
+        graficoDonut.update();
+        updateTextoPorcentagem();
+        setTimeout(animacaoProgresso, 10);
+    };
 
-    setTimeout(animacaoProgresso, 10);
+    animacaoProgresso();
 }
